74a704c2adb92cdf6a9aa954a846646d21008fb0
xuyuan23 <643854343@qq.com>
2023-08-11 17:53:27 +0800

    init project Text2Video

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # Text2Video
 Text2Video Multi-model Experience Center
+
+
+## Supported LLMs
+
+| Video LLMs            | Supported   | Model Type | Notes |
+|-----------------------|-------------|------------|-------|
+| zeroscope_v2_576w     | Yes         | Local      |       |
+| text-to-video-ms-1.7b | Yes         | Local      |       |
+| Gen-2                 | Coming soon | Proxy      |       |
+
+## Installation
+
+- Download video models from huggingface, and install the relevant dependencies.
+
+```commandline
+git lfs install
+git clone https://huggingface.co/damo-vilab/text-to-video-ms-1.7b
+
+# git lfs install
+# git clone https://huggingface.co/cerspense/zeroscope_v2_576w
+
+pip install -r requirements.txt
+```
+
+- start project.
+
+```commandline
+
+# Start in Local environment.
+python main.py
+
+# Start in Pord environment, you should modify param `OUT_IP` in main_prod.py.
+nohup python main_prod.py > text2video.log &
+
+```
+
+## Configuration
+
+- you can change the model in main.py, `zeroscope_v2_576w` is default.
+
+```python
+
+VIDEO_MODEL = "zeroscope_v2_576w"
+# VIDEO_MODEL = "text-to-video-ms-1.7b"
+```
+
+## Usage
+
+```commandline
+curl -X POST -H "Content-Type: application/json" -d '{
+"prompt": "A beautiful girl walks through the mall with a cup of milk tea, her hair blowing in the wind",
+"height": 200,
+"width": 576,
+"num_frames": 24
+}' http://120.27.193.115:7861/generate_video
+```
+
+```json
+{
+    "success": true,
+    "msg": "http://120.27.193.115:7861/girl.mp4"
+}
+```
+
+
---
+++ main_prod.py
@@ -0,0 +1,58 @@
+import os
+import time
+
+import torch
+import uvicorn
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers.utils import export_to_video
+from fastapi import FastAPI
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from pydantic import BaseModel
+
+app = FastAPI()
+
+VIDEO_MODEL = "zeroscope_v2_576w"
+# VIDEO_MODEL = "text-to-video-ms-1.7b"
+
+OUT_IP = "127.0.0.1"
+# OUT_IP = "{Your-server-IP}"
+
+# the image generated path, default in /var/www/html, if you start a httpd service, you can access by {OUT_IP/video.mp4}
+GENERATED_IMAGES_PATH = "/var/www/html"
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+MODEL_PATH = os.path.join(ROOT_PATH, "models")
+
+pipe = DiffusionPipeline.from_pretrained(os.path.join(MODEL_PATH, VIDEO_MODEL), torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.enable_model_cpu_offload()
+
+
+class LLMPrompt(BaseModel):
+    prompt: str = None
+    num_inference_steps: int = 40
+    height: int = 320
+    width: int = 576
+    num_frames: int = 24
+
+
+@app.post("/generate_video")
+def generate_video(lp: LLMPrompt):
+
+    try:
+        video_frames = pipe(lp.prompt, lp.num_inference_steps, lp.height, lp.width, lp.num_frames).frames
+        timestamp = int(time.time())
+        video_name_tmp = f"{VIDEO_MODEL}_{str(timestamp)}_tmp.mp4"
+        video_path = export_to_video(video_frames, os.path.join(DATA_PATH, video_name_tmp))
+        video = VideoFileClip(video_path)
+
+        video_name = f"{VIDEO_MODEL}_{str(timestamp)}.mp4"
+        video.write_videofile(os.path.join(GENERATED_IMAGES_PATH, video_name), codec="libx264", audio_codec="aac")
+        return {"success": True, "msg": f"http://{OUT_IP}/{video_name}"}
+    except Exception as e:
+        return {"success": False, "msg": f"generate error: {str(e)}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=7861, log_level="info")
