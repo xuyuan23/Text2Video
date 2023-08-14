95ae9d6d5c5d86fc75cd78ea5046da9dfd7eb559
xuyuan23 <643854343@qq.com>
2023-08-14 15:19:10 +0800

    using 'black .' to format code.

--- main_prod.py
+++ main_prod.py
@@ -24,7 +24,9 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(ROOT_PATH, "data")
 MODEL_PATH = os.path.join(ROOT_PATH, "models")
 
-pipe = DiffusionPipeline.from_pretrained(os.path.join(MODEL_PATH, VIDEO_MODEL), torch_dtype=torch.float16)
+pipe = DiffusionPipeline.from_pretrained(
+    os.path.join(MODEL_PATH, VIDEO_MODEL), torch_dtype=torch.float16
+)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe.enable_model_cpu_offload()
 
@@ -34,24 +36,35 @@ if not os.path.exists(DATA_PATH):
 
 class LLMPrompt(BaseModel):
     prompt: str = None
-    num_inference_steps: int = 40
+    num_inference_steps: int = 25
     height: int = 320
-    width: int = 576
-    num_frames: int = 24
+    width: int = 480
+    num_frames: int = 40
 
 
 @app.post("/generate_video")
 def generate_video(lp: LLMPrompt):
-
     try:
-        video_frames = pipe(prompt=lp.prompt, num_inference_steps=lp.num_inference_steps, height=lp.height, width=lp.width, num_frames=lp.num_frames).frames
+        video_frames = pipe(
+            prompt=lp.prompt,
+            num_inference_steps=lp.num_inference_steps,
+            height=lp.height,
+            width=lp.width,
+            num_frames=lp.num_frames,
+        ).frames
         timestamp = int(time.time())
         video_name_tmp = f"{VIDEO_MODEL}_{str(timestamp)}_tmp.mp4"
-        video_path = export_to_video(video_frames, os.path.join(DATA_PATH, video_name_tmp))
+        video_path = export_to_video(
+            video_frames, os.path.join(DATA_PATH, video_name_tmp)
+        )
         video = VideoFileClip(video_path)
 
         video_name = f"{VIDEO_MODEL}_{str(timestamp)}.mp4"
-        video.write_videofile(os.path.join(GENERATED_IMAGES_PATH, video_name), codec="libx264", audio_codec="aac")
+        video.write_videofile(
+            os.path.join(GENERATED_IMAGES_PATH, video_name),
+            codec="libx264",
+            audio_codec="aac",
+        )
         return {"success": True, "msg": f"http://{OUT_IP}/{video_name}"}
     except Exception as e:
         return {"success": False, "msg": f"generate error: {str(e)}"}
